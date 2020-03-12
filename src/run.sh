6769319a95fc26f469f58cddc8db444f710a737b
yzhangdx <rookiecoder.z@gmail.com>
2020-03-12 14:00:30 +0800

    an example of training and evaluation

--- src/run.sh
+++ src/run.sh
@@ -1,13 +1,15 @@
 
 #!/bin/bash
 
+# an example of training and evaluation of the domain adaptation model. 
+
 python train_feature.py --gpu_id <gpu_id> \
                         --net ResNet50 \
                         --dset office \
                         --test_interval 500 \
                         --snapshot_interval 10000 \
                         --ly_type cosine \
-                        --loss_type coral \
+                        --loss_type mmd \
                         --fisher_loss_type tr \
                         --output_dir <output_path> \
                         --s_dset_path ../data/office31/amazon_31_list.txt \
@@ -15,7 +17,7 @@ python train_feature.py --gpu_id <gpu_id> \
                         --em_loss_coef 0.1 \
                         --inter_loss_coef 1. \
                         --intra_loss_coef 1. \
-                        --trade_off 10.
+                        --trade_off 1.
 
 python eval_da.py --gpu_id <gpu_id> \
                   --net ResNet50 \
